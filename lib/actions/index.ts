28698c9bcd9e135b3ef0e4bdeeead24df4bf86ce
baibhav <baibhavtiwari37@gmail.com>
2023-10-15 00:53:55 +0530

    Product Details Page ✅

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
-import { scrapeAmazonProduct } from "../scraper";
+import { scarpeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
 
@@ -13,7 +13,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
   try {
     connectToDB();
 
-    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+    const scrapedProduct = await scarpeAmazonProduct(productUrl);
 
     if (!scrapedProduct) return;
 
