d5a13eef59af9e0397324c72ce4fbef901cebbb1
baibhav <baibhavtiwari37@gmail.com>
2023-10-16 16:29:52 +0530

    Email Funcionality

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,7 @@ import { connectToDB } from "../mongoose";
 import { scarpeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -92,4 +93,30 @@ export async function getSimilarProducts(productId: string) {
   }
 }
 
+export async function addUserEmailToProduct(
+  productId: string,
+  userEmail: string
+) {
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) return;
+
+    const userExists = product.users.some(
+      (user: User) => user.email === userEmail
+    );
+
+    if (!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = await generateEmailBody(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
